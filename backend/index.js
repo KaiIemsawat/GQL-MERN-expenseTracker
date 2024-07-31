3426c497e6d892466181cd30fb4945bfbaa003ab
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-31 11:21:54 -0400

    connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 
 import mergeResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
+import { connectDB } from "./db/connectDB.js";
 
 dotenv.config();
 
@@ -41,5 +42,6 @@ app.use(
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
 
 console.log(`🚀 Server ready at http://localhost:4000`);

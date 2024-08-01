bddba98c1959cd16b3f7e5216afd7741b376af2b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-07-31 22:08:44 -0400

    query transaction(s)

--- backend/resolvers/transactionResolver.js
+++ backend/resolvers/transactionResolver.js
@@ -1,5 +1,32 @@
+import Transaction from "../models/transactionModel.js";
+
 const transactionResolver = {
-    Query: {},
+    Query: {
+        transactions: async (_, _, context) => {
+            try {
+                if (!context.getUser()) {
+                    throw new Error("Unauthorized");
+                }
+                const userId = await context.getUser()._id;
+
+                const transactions = await Transaction.find({ userId });
+                return transactions;
+            } catch (error) {
+                console.error("Error getting transactions : ", error);
+                throw new Error("Error getting transactions");
+            }
+        },
+        transaction: async (_, { transactionId }) => {
+            try {
+                const transaction = await Transaction.findById(transactionId);
+                return transaction;
+            } catch (error) {
+                console.error("Error getting transaction : ", error);
+                throw new Error("Error getting transaction");
+            }
+        },
+        // TODO => ADD category statistics query
+    },
     Mutation: {},
 };
 
--- backend/typeDefs/transactionTypeDef.js
+++ backend/typeDefs/transactionTypeDef.js
@@ -13,6 +13,7 @@ const transactionTypeDef = `#graphql
   type Query {
     transactions: [Transaction!]
     transaction(transactionId: ID!): Transaction
+    # TODO => ADD category statistics query
   }
 
   type Mutation {

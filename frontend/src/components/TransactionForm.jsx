8d79bbcd29ac8033d7f60bb0b55698a8af20ed97
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-02 18:57:52 -0400

    Homepage

---
+++ frontend/src/components/TransactionForm.jsx
@@ -0,0 +1,167 @@
+const TransactionForm = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const transactionData = {
+      // formData.get("name of element")
+      description: formData.get("description"),
+      paymentType: formData.get("paymentType"),
+      category: formData.get("category"),
+      amount: parseFloat(formData.get("amount")),
+      location: formData.get("location"),
+      date: formData.get("date"),
+    };
+    console.log("transactionData", transactionData);
+  };
+
+  return (
+    <form
+      className="flex w-full max-w-lg flex-col gap-5 px-3"
+      onSubmit={handleSubmit}
+    >
+      {/* TRANSACTION */}
+      <div className="flex flex-wrap">
+        <div className="w-full">
+          <label
+            className="mb-2 block text-xs font-bold uppercase tracking-wide text-white"
+            htmlFor="description"
+          >
+            Transaction
+          </label>
+          <input
+            className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+            id="description"
+            name="description"
+            type="text"
+            required
+            placeholder="Rent, Groceries, Salary, etc."
+          />
+        </div>
+      </div>
+      {/* PAYMENT TYPE */}
+      <div className="flex flex-wrap gap-3">
+        <div className="mb-6 w-full flex-1 md:mb-0">
+          <label
+            className="mb-2 block text-xs font-bold uppercase tracking-wide text-white"
+            htmlFor="paymentType"
+          >
+            Payment Type
+          </label>
+          <div className="relative">
+            <select
+              className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+              id="paymentType"
+              name="paymentType"
+            >
+              <option value={"card"}>Card</option>
+              <option value={"cash"}>Cash</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="h-4 w-4 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        {/* CATEGORY */}
+        <div className="mb-6 w-full flex-1 md:mb-0">
+          <label
+            className="mb-2 block text-xs font-bold uppercase tracking-wide text-white"
+            htmlFor="category"
+          >
+            Category
+          </label>
+          <div className="relative">
+            <select
+              className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+              id="category"
+              name="category"
+            >
+              <option value={"saving"}>Saving</option>
+              <option value={"expense"}>Expense</option>
+              <option value={"investment"}>Investment</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="h-4 w-4 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        {/* AMOUNT */}
+        <div className="mb-6 w-full flex-1 md:mb-0">
+          <label
+            className="mb-2 block text-xs font-bold uppercase text-white"
+            htmlFor="amount"
+          >
+            Amount($)
+          </label>
+          <input
+            className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+            id="amount"
+            name="amount"
+            type="number"
+            placeholder="150"
+          />
+        </div>
+      </div>
+
+      {/* LOCATION */}
+      <div className="flex flex-wrap gap-3">
+        <div className="mb-6 w-full flex-1 md:mb-0">
+          <label
+            className="mb-2 block text-xs font-bold uppercase tracking-wide text-white"
+            htmlFor="location"
+          >
+            Location
+          </label>
+          <input
+            className="mb-3 block w-full appearance-none rounded border bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+            id="location"
+            name="location"
+            type="text"
+            placeholder="New York"
+          />
+        </div>
+
+        {/* DATE */}
+        <div className="w-full flex-1">
+          <label
+            className="mb-2 block text-xs font-bold uppercase tracking-wide text-white"
+            htmlFor="date"
+          >
+            Date
+          </label>
+          <input
+            type="date"
+            name="date"
+            id="date"
+            className="mb-3 block w-full appearance-none rounded border bg-gray-200 px-4 py-[11px] leading-tight text-gray-700 focus:bg-white focus:outline-none"
+            placeholder="Select date"
+          />
+        </div>
+      </div>
+      {/* SUBMIT BUTTON */}
+      <button
+        className="w-full rounded bg-gradient-to-br from-pink-500 to-pink-500 px-4 py-2 font-bold text-white hover:from-pink-600 hover:to-pink-600 disabled:cursor-not-allowed disabled:opacity-70"
+        type="submit"
+      >
+        Add Transaction
+      </button>
+    </form>
+  );
+};
+
+export default TransactionForm;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,77 @@
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+import Cards from "../components/Cards";
+import TransactionForm from "../components/TransactionForm";
+
+import { MdLogout } from "react-icons/md";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const chartData = {
+    labels: ["Saving", "Expense", "Investment"],
+    datasets: [
+      {
+        label: "%",
+        data: [13, 8, 3],
+        backgroundColor: [
+          "rgba(75, 192, 192)",
+          "rgba(255, 99, 132)",
+          "rgba(54, 162, 235)",
+        ],
+        borderColor: [
+          "rgba(75, 192, 192)",
+          "rgba(255, 99, 132)",
+          "rgba(54, 162, 235, 1)",
+        ],
+        borderWidth: 1,
+        borderRadius: 30,
+        spacing: 10,
+        cutout: 130,
+      },
+    ],
+  };
+
+  const handleLogout = () => {
+    console.log("Logging out...");
+  };
+
+  const loading = false;
+
+  return (
+    <>
+      <div className="relative z-20 mx-auto flex max-w-7xl flex-col items-center justify-center gap-6">
+        <div className="flex items-center">
+          <p className="relative z-50 mb-4 mr-4 inline-block bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-400 bg-clip-text text-center text-2xl font-bold text-transparent md:text-4xl lg:text-4xl">
+            Spend wisely, track wisely
+          </p>
+          <img
+            src={"https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
+            className="h-11 w-11 cursor-pointer rounded-full border"
+            alt="Avatar"
+          />
+          {!loading && (
+            <MdLogout
+              className="mx-2 h-5 w-5 cursor-pointer"
+              onClick={handleLogout}
+            />
+          )}
+          {/* loading spinner */}
+          {loading && (
+            <div className="mx-2 h-6 w-6 animate-spin rounded-full border-b-2 border-t-2"></div>
+          )}
+        </div>
+        <div className="flex w-full flex-wrap items-center justify-center gap-6">
+          <div className="h-[330px] w-[330px] md:h-[360px] md:w-[360px]">
+            <Doughnut data={chartData} />
+          </div>
+
+          <TransactionForm />
+        </div>
+        <Cards />
+      </div>
+    </>
+  );
 };
 export default HomePage;
